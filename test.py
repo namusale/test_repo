ef68e58ea3fcdaf21e612a468b3c61e989118ede
Namusale Chama <namusale@gmail.com>
2023-12-26 23:39:46 +0000

    simple code

---
+++ test.py
@@ -0,0 +1,10 @@
+'''This code works to do simple addtions'''
+
+print('')
+number1 = int(input("Enter a number please. "))
+number2 = int(input("Enter a another number please. "))
+
+
+print('')
+print(f'The sum is {number1 + number2}')
+print('')
